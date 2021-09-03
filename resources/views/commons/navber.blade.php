23bfc45681c0d821c4886fcb6463d4bd9e633d73
Ryosuke <sessyomaru427@gmail.com>
2021-09-03 22:40:58 +0900

    ユーザー登録

--- resources/views/commons/navber.blade.php
+++ resources/views/commons/navber.blade.php
@@ -11,7 +11,7 @@
             <ul class="navber-nav mr-auto"></ul>
             <ul class="navber-nav">
                 {{-- ユーザ登録ページへのリンク --}}
-                <li class="navber-item"><a href="#" class="nav-link">Signup</a></li>
+                <li>{!! link_to_route('signup.get','Signup',[],['class'=>'nav-link']) !!}</li>
                 {{-- ログインページへのリンク --}}
                 <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
             </ul>
