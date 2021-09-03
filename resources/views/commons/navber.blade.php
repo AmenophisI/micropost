d2365f94e076aa32b8bc46ce78f2e144c94edace
Ryosuke <sessyomaru427@gmail.com>
2021-09-04 06:22:33 +0900

    ユーザーログイン

--- resources/views/commons/navber.blade.php
+++ resources/views/commons/navber.blade.php
@@ -1,19 +1,34 @@
 <header class="mb-4">
-    <nav class="navber navber-expand-sm navber-dark bg-dark">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         {{-- トップページへのリンク --}}
-        <a class="navber-brand" href="/">Microposts</a>
+        <a class="navbar-brand" href="/">Microposts</a>
 
-        <button type="button" class="navber-toggler" data-toggle="collapse" data-target="#nav-ber">
-            <span class="navber-toggler-icon"></span>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navber-collapse" id="nav-ber">
-            <ul class="navber-nav mr-auto"></ul>
-            <ul class="navber-nav">
-                {{-- ユーザ登録ページへのリンク --}}
-                <li>{!! link_to_route('signup.get','Signup',[],['class'=>'nav-link']) !!}</li>
-                {{-- ログインページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                @if (Auth::check())
+                    {{-- ユーザ一覧ページへのリンク --}}
+                    <li class="nav-item"><a href="#" class="nav-link">Users</a></li>
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            {{-- ユーザ詳細ページへのリンク --}}
+                            <li class="dropdown-item"><a href="#">My profile</a></li>
+                            <li class="dropdown-divider"></li>
+                            {{-- ログアウトへのリンク --}}
+                            <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                        </ul>
+                    </li>
+                @else
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログインページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
+                @endif
             </ul>
         </div>
     </nav>
