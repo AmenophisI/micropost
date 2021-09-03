1b700e05371d37a26224afd2099228854f547fc4
Ryosuke <sessyomaru427@gmail.com>
2021-09-03 15:51:59 +0900

    トップページ

---
+++ resources/views/commons/navber.blade.php
@@ -0,0 +1,20 @@
+<header class="mb-4">
+    <nav class="navber navber-expand-sm navber-dark bg-dark">
+        {{-- トップページへのリンク --}}
+        <a class="navber-brand" href="/">Microposts</a>
+
+        <button type="button" class="navber-toggler" data-toggle="collapse" data-target="#nav-ber">
+            <span class="navber-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navber-collapse" id="nav-ber">
+            <ul class="navber-nav mr-auto"></ul>
+            <ul class="navber-nav">
+                {{-- ユーザ登録ページへのリンク --}}
+                <li class="navber-item"><a href="#" class="nav-link">Signup</a></li>
+                {{-- ログインページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
